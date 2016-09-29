fe22f24f7fb2d4707e18c57985cb84f0b76197b1
wanghui <wanghui_whwx@sina.com>
2016-09-29 16:44:21 +0800

    修改评论获取

--- src/com/woting/discuss/web/DiscussController.java
+++ src/com/woting/discuss/web/DiscussController.java
@@ -246,7 +246,7 @@ public class DiscussController {
             if (ol!=null&&ol.size()>0) {
                 map.put("ReturnType", "1001");
                 map.put("AllCount", ol.get("AllCount"));
-                map.put("OpinionList", ol.get("List"));
+                map.put("OpinionList", convertDiscissView((List<Discuss>)(ol.get("List"))));
             } else {
                 map.put("ReturnType", "1011");
                 map.put("Message", "无评论信息");
