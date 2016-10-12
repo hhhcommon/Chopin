3f6254d8fd12a07942fa317de1f5b8d1ae715354
wanghui <wanghui_whwx@sina.com>
2016-10-12 13:26:54 +0800

    完成首页自动登录

--- src/com/woting/passport/session/service/RedisSessionService.java
+++ src/com/woting/passport/session/service/RedisSessionService.java
@@ -98,7 +98,7 @@ public class RedisSessionService implements SessionService {
                 } else {//不是User，自动登录
                     if (operDesc.equals("common/entryApp")||udk.getPCDType()==3) {//自动登录
                         MobileUsedPo mup=muService.getUsedInfo(udk.getDeviceId(), udk.getPCDType());
-                        if (mup.getStatus()==1) {//自动登录
+                        if (mup!=null&&mup.getStatus()==1) {//自动登录
                             rUdk.setUserId(mup.getUserId());
                             udk.setUserId(mup.getUserId());
                             conn.set(rUdk.getKey_UserLoginStatus().getBytes(), (System.currentTimeMillis()+"::register").getBytes());
